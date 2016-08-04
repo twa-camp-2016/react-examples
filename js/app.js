be902024863dcbfd1f55b1c115ce961b51190c25
code <1165444511@qq.com>
2016-08-04 11:07:36 +0800

    helloworld can works

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-// TODO
+const App = React.createClass({
+    getInitialState: function () {
+        return {
+            result: "hello"
+        }
+    },
+    render: function () {
+        return <div> {this.state.result}
+            <div>
+                <Show outPut="world"/>
+            </div>
+        </div>
+    }
+})
+
+const Show = React.createClass({
+    render: function () {
+        return <div> {this.props.outPut}</div>
+    }
+})
+
+ReactDOM.render(<App />, document.getElementById('content'));
