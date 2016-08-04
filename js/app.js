db426918f98f117b82355ed4df82ef40b78c199e
gjieoo <15202979397@163.com>
2016-08-04 23:52:07 +0800

    jump of the page between editor and preview

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 const App=React.createClass({
-
+    getInitialState:function () {
+        return {
+            isEditor:true
+        }
+    },
+    displayEditor:function(){
+        this.setState({isEditor:!this.state.isEditor})
+    },
     render:function(){
-
+            const isEditor=this.state.isEditor;
             return <div>
-                <button>预览</button>
-                <Editor/>
-                <Preview/>
+                <button onClick={this.displayEditor}>{isEditor?'预览':'编辑'}</button>
+                <div className={isEditor?"":"hidden"}>
+                    <Editor/>
+                </div>
+                <div className={isEditor?"hidden":""}>
+                    <Preview />
+                </div>
+
             </div>
 
     }
