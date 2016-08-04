a36d4188030f93d5cf304079f03ae944422f1b05
gjieoo <15202979397@163.com>
2016-08-05 00:51:44 +0800

    add text or date

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
 const App=React.createClass({
     getInitialState:function () {
         return {
-            isEditor:true
+            isEditor:true,
+            elements:[]
         }
     },
     displayEditor:function(){
         this.setState({isEditor:!this.state.isEditor})
+    },
+    addElement:function(element){
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements})
     },
     render:function(){
             const isEditor=this.state.isEditor;
             return <div>
                 <button onClick={this.displayEditor}>{isEditor?'预览':'编辑'}</button>
                 <div className={isEditor?"":"hidden"}>
-                    <Editor/>
+                    <Editor elements={this.state.elements} onAdd={this.addElement} onDelete={this.deleteElement}/>
                 </div>
                 <div className={isEditor?"hidden":""}>
-                    <Preview />
+                    <Preview elements={this.state.elements}/>
                 </div>
 
             </div>
@@ -25,24 +31,40 @@ const App=React.createClass({
 const Editor=React.createClass({
     render:function(){
         return <div>
-            <Left/>
-            <Right/>
+            <Left elements={this.props.elements} onDelete={this.props.onDelete}/>
+            <Right onAdd={this.props.onAdd}/>
         </div>
 
     }
 });
 const Left=React.createClass({
     render:function(){
+        const  elements=this.props.elements.map((element,index)=>{
+            return(
+                <div key={index}>
+                    <input type={element}/>
+                    <button>X</button>
+                </div>
+            )
+
+        });
+
         return <div>
-            Left
+            {elements}
         </div>
 
     }
 });
 const Right=React.createClass({
+    add:function(){
+        const element=$("input[name=element]:checked").val();
+        this.props.onAdd(element);
+    },
     render:function(){
         return <div>
-            Right
+            <input type="radio" name="element" value="text"/>文本框
+            <input type="radio" name="element" value="date"/>日期
+            <button onClick={this.add}>+</button>
         </div>
 
     }
