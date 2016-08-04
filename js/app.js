44a09d521057365d15b6fe67074e73557e53fc82
gjieoo <15202979397@163.com>
2016-08-05 07:36:53 +0800

    it is works

--- js/app.js
+++ js/app.js
@@ -1,97 +1,10 @@
 const App=React.createClass({
-    getInitialState:function () {
-        return {
-            isEditor:true,
-            elements:[]
-        }
-    },
-    displayEditor:function(){
-        this.setState({isEditor:!this.state.isEditor})
-    },
-    addElement:function(element){
-        const elements = this.state.elements;
-        elements.push(element);
-        this.setState({elements})
-    },
-    deleteElement:function(index){
-        const elements = this.state.elements;
-        elements.splice(index,1);
-        this.setState({elements})
-    },
-    render:function(){
-            const isEditor=this.state.isEditor;
-            return <div>
-                <button onClick={this.displayEditor}>{isEditor?'预览':'编辑'}</button>
-                <div className={isEditor?"":"hidden"}>
-                    <Editor elements={this.state.elements} onAdd={this.addElement} onDelete={this.deleteElement}/>
-                </div>
-                <div className={isEditor?"hidden":""}>
-                    <Preview elements={this.state.elements}/>
-                </div>
+   render:function(){
+       return <div>
+           it is work
+       </div>
 
-            </div>
-
-    }
-});
-const Editor=React.createClass({
-    render:function(){
-        return <div>
-            <Left elements={this.props.elements} onDelete={this.props.onDelete}/>
-            <Right onAdd={this.props.onAdd}/>
-        </div>
-
-    }
-});
-const Left=React.createClass({
-    remove:function(index){
-
-        this.props.onDelete(index);
-    },
-    render:function(){
-        const  elements=this.props.elements.map((element,index)=>{
-            return(
-                <div key={index}>
-                    <input type={element}/>
-                    <button onClick={this.remove.bind(this,index)}>X</button>
-                </div>
-            )
-
-        });
-
-        return <div>
-            {elements}
-        </div>
-
-    }
-});
-const Right=React.createClass({
-    add:function(){
-        const element=$("input[name=element]:checked").val();
-        this.props.onAdd(element);
-    },
-    render:function(){
-        return <div>
-            <input type="radio" name="element" value="text"/>文本框
-            <input type="radio" name="element" value="date"/>日期
-            <button onClick={this.add}>+</button>
-        </div>
-
-    }
-});
-const Preview=React.createClass({
-    render:function(){
-        const  elements=this.props.elements.map((element,index)=>{
-            return(
-                <div key={index}>
-                    <input type={element}/>
-                </div>
-            )});
-        return <div>
-            {elements}
-            <button>提交</button>
-        </div>
-
-    }
+   }
 });
 
-ReactDOM.render(<App />, document.getElementById('content'));
+ReactDOM.render(<App/>,document.getElementById('content'));
