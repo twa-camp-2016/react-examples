55c2d5ef422db41894b807b19a8ee79cd657925c
gjieoo <15202979397@163.com>
2016-08-05 10:00:20 +0800

    add elements

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 const App=React.createClass({
     getInitialState:function () {
       return {
-          isEditor:true
+          isEditor:true,
+          elements:[]
       }
     },
     displayEditor:function () {
         this.setState({isEditor:!this.state.isEditor});
+    },
+    addElements:function (element) {
+        const elements=this.state.elements;
+        elements.push(element);
+        this.setState({elements})
     },
     render:function(){
         const isEditor=this.state.isEditor;
         return <div>
             <button onClick={this.displayEditor}>{isEditor?'preview':'editor'}</button>
             <div className={isEditor?'':'hidden'}>
-                <Editor/>
+                <Editor elements={this.state.elements} onAdd={this.addElements}/>
             </div>
             <div className={isEditor?'hidden':''}>
                 <Preview/>
@@ -26,22 +32,35 @@ const App=React.createClass({
 const Editor=React.createClass({
    render:function(){
       return <div>
-          <Left/>
-          <Right/>
+          <Left elements={this.props.elements}/>
+          <Right onAdd={this.props.onAdd}/>
       </div>
    }
 });
 const Left=React.createClass({
     render:function(){
+        const elements=this.props.elements.map((ele,index)=>{
+            return <div key={index}>
+                <input type={ele}/>
+                <button>X</button>
+            </div>
+        });
         return <div>
-            Left
+            {elements}
         </div>
     }
 });
 const Right=React.createClass({
+    add:function () {
+        const elements=$('input[name=element]:checked').val();
+        this.props.onAdd(elements)
+    },
     render:function(){
+
         return <div>
-            Right
+            <input type="radio" name="element" value="text"/>text
+            <input type="radio" name="element" value="date"/>date
+            <button onClick={this.add}>+</button>
         </div>
     }
 });
