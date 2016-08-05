1eb579ad7dfaf8c8534e23d13099caa53553ba6f
gjieoo <15202979397@163.com>
2016-08-05 08:33:34 +0800

    add elements

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const App=React.createClass({
     getInitialState:function(){
         return ({
-            isEditor:true
+            isEditor:true,
+            elements:[]
         })
 
     },
@@ -10,12 +11,20 @@ const App=React.createClass({
         this.setState({isEditor:!isEditor})
 
     },
+    addElement:function (element) {
+      const elements=this.state.elements;
+        elements.push(element);
+        this.setState({
+            elements
+        });
+    },
    render:function(){
        const isEditor=this.state.isEditor;
+       const elements=this.state.elements;
        return <div>
            <button onClick={this.display}>{isEditor?'预览':'编辑'}</button>
            <div className={isEditor?'':'hidden'}>
-               <Editor/>
+               <Editor elements={elements} onAdd={this.addElement}/>
            </div>
            <div className={isEditor?'hidden':''}>
                <Preview/>
@@ -28,24 +37,36 @@ const App=React.createClass({
 const Editor=React.createClass({
     render:function(){
         return <div>
-            <Left/>
-            <Right/>
+            <Left elements={this.props.elements}/>
+            <Right onAdd={this.props.onAdd}/>
         </div>
 
     }
 });
 const Left=React.createClass({
     render:function(){
+        const elements=this.props.elements.map((ele,index)=>{
+           return <div key={index}>
+               <input type={ele}/>
+               <button>X</button>
+           </div>
+        });
         return <div>
-            Left
+            {elements}
         </div>
 
     }
 });
 const Right=React.createClass({
+    add:function(){
+        const element=$('input[name=element]:checked').val()
+        this.props.onAdd(element);
+    },
     render:function(){
         return <div>
-            Right
+            <input type="radio" name="element" value="text"/>text
+            <input type="radio" name="element" value="date"/>date
+            <button onClick={this.add}>+</button>
         </div>
 
     }
