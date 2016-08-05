80fc411d1898456d5874043ed4f34f577d313ab9
gjieoo <15202979397@163.com>
2016-08-05 09:31:26 +0800

    add elements

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
 const App=React.createClass({
     getInitialState:function () {
       return {
-          isEditor:true
+          isEditor:true,
+          elements:[]
       }
     },
     displayEditor:function () {
       const isEditor=this.state.isEditor;
         this.setState({isEditor:!isEditor})
     },
+    addElements:function (element) {
+        const elements=this.state.elements;
+        elements.push(element);
+        this.setState({
+            elements
+        });
+    },
    render:function(){
        const isEditor=this.state.isEditor;
        return <div>
            <button onClick={this.displayEditor}>{isEditor?'预览':'编辑'}</button>
            <div className={isEditor?'':"hidden"}>
-               <Editor/>
+               <Editor elements={this.state.elements} onAdd={this.addElements}/>
            </div>
            <div className={isEditor?'hidden':""}>
                <Preview/>
@@ -27,22 +35,36 @@ const App=React.createClass({
 const Editor=React.createClass({
    render:function () {
        return <div>
-           <Left/>
-           <Right/>
+           <Left elements={this.props.elements}/>
+           <Right onAdd={this.props.onAdd}/>
        </div>
    }
 });
 const Left=React.createClass({
+
     render:function () {
+        const elements=this.props.elements.map((ele,index)=>{
+            return <div key={index}>
+                <input type={ele}/>
+                <button>X</button>
+            </div>
+        });
         return <div>
-            Left
+            {elements}
         </div>
     }
 });
 const Right=React.createClass({
+    add:function () {
+      const element=$('input[name=element]:checked').val();
+        this.props.onAdd(element);
+    },
     render:function () {
+
         return <div>
-            Right
+            <input type="radio" name="element" value="text"/>text
+            <input type="radio" name="element" value="date"/>date
+            <button onClick={this.add}>+</button>
         </div>
     }
 });
