7c68093276a70b37790b2d8d99cbeae046937650
gjieoo <15202979397@163.com>
2016-08-05 09:43:46 +0800

    add Editor and preview compontent

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
 const App=React.createClass({
     render:function(){
         return <div>
-            it is work
+            <button>preview</button>
+            <Editor/>
+            <Preview/>
         </div>
 
+    }
+});
+
+const Editor=React.createClass({
+   render:function(){
+      return <div>
+          Editor
+      </div>
+   }
+});
+
+const Preview=React.createClass({
+    render:function(){
+        return <div>
+            Preview
+        </div>
     }
 });
 
