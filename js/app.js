579f19829f4bfa440ee19355f4aa3f38f99ffd97
gjieoo <15202979397@163.com>
2016-08-05 09:50:01 +0800

    add Left and righe component

--- js/app.js
+++ js/app.js
@@ -26,11 +26,25 @@ const App=React.createClass({
 const Editor=React.createClass({
    render:function(){
       return <div>
-          Editor
+          <Left/>
+          <Right/>
       </div>
    }
 });
-
+const Left=React.createClass({
+    render:function(){
+        return <div>
+            Left
+        </div>
+    }
+});
+const Right=React.createClass({
+    render:function(){
+        return <div>
+            Right
+        </div>
+    }
+});
 const Preview=React.createClass({
     render:function(){
         return <div>
